760510c2099ca0a9e40131610e0fdbb2cb5fd83d
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2018-11-04 00:07:11 +0530

    server side graphql with mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const graphQLHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema');
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/graphql");
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB");
+
+});
+
 app.use('/graphql', graphQLHTTP({
-    schema: schema
+    schema: schema,
+    graphiql: true
 }));
 
 app.listen(4000, () => {
